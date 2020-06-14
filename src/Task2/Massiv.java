f7807e7ff7c85fb7854d18a6cef165222db150f4
Ivan Kargapolov <wixz@yandex.ru>
2020-06-14 20:07:12 +0500

    ВТорое залдание

---
+++ src/Task2/Massiv.java
@@ -0,0 +1,11 @@
+package Task2;
+
+public class Massiv {
+    int arr[];
+
+    public Massiv(int a[]) {
+        arr = a;
+    }
+    public void add(int a, int index) {
+    }
+}
---
+++ src/Task2/Task_2.java
@@ -0,0 +1,7 @@
+package Task2;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
